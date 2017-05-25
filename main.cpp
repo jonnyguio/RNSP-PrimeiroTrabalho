ee7c9e87ee54541ea490b4e1e01839604ff6ce89
João Luis da Silva Guio Soares <joaoluisguio@gmail.com>
2017-05-25 16:44:45 -0300

    Trying to solve threads problem.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,23 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <thread>
 
-const static int threshold = 122;
+const int numThreads = 4;
+const int numBitsAddrs = 2;
+const int threshold = 122;
+const bool bleaching = true;
 
 using namespace wann;
+
+void executePartial(std::vector<std::vector<int>> array, std::vector<std::vector<std::string>> &result, WiSARD w) {
+    std::vector<std::string> res;
+    std::cout << "Starting prediction..." << std::endl;
+    res = w.predict(array);
+    std::cout << "Finished predict, adding to list" << std::endl;
+    result.push_back(res);
+    std::cout << "Exiting..." << std::endl;
+}
 
 void reverseInt(int *input) {
     unsigned char c1, c2, c3, c4;
@@ -80,11 +93,21 @@ int main(int argc, char *argv[]) {
     int count = 0;
     std::vector<std::vector<int>> trainingImages, testImages;
     std::vector<std::string> trainingLabels, testLabels, result;
-    
-    readLabelsMNIST("./mnist/train-labels-idx1-ubyte", trainingLabels);
-    readImagesMNIST("./mnist/train-images-idx3-ubyte", trainingImages);
-    readLabelsMNIST("./mnist/t10k-labels-idx1-ubyte", testLabels);
-    readImagesMNIST("./mnist/t10k-images-idx3-ubyte", testImages);
+    std::vector<std::vector<int>>::const_iterator first, last;
+    std::vector<std::thread *> prediction;
+    std::vector<std::vector<std::string>> resultPartial;
+
+    const char *trainLabelsFilename = "./mnist/train-labels-idx1-ubyte", *trainImagesFilename = "./mnist/train-images-idx3-ubyte", *testLabelsFilename = "./mnist/t10k-labels-idx1-ubyte", *testImagesFilename = "./mnist/t10k-images-idx3-ubyte";
+
+    std::thread tTrainLabels(readLabelsMNIST, std::ref(trainLabelsFilename), std::ref(trainingLabels));
+    std::thread tTrainImages(readImagesMNIST, std::ref(trainImagesFilename), std::ref(trainingImages));
+    std::thread tTestLabels(readLabelsMNIST, std::ref(testLabelsFilename), std::ref(testLabels));
+    std::thread tTestImages(readImagesMNIST, std::ref(testImagesFilename), std::ref(testImages));
+
+    tTrainLabels.join();
+    tTrainImages.join();
+    tTestLabels.join();
+    tTestImages.join();
 
     std::cout << "Preparing it to WiSARD..." << std::endl;
     for (int i = 0; i < trainingImages.size(); i++) {
@@ -98,7 +121,7 @@ int main(int argc, char *argv[]) {
         }
     }
     
-    WiSARD *w = new WiSARD(trainingImages[0].size(), 2);
+    WiSARD *w = new WiSARD(trainingImages[0].size(), numBitsAddrs, bleaching);
 
     // std::cout << trainingImages[0].size() << " " << trainingImages.size() << " " << trainingLabels.size() << std::endl;
 
@@ -106,14 +129,33 @@ int main(int argc, char *argv[]) {
     w->fit(trainingImages, trainingLabels);
 
     std::cout << "Prediction..." << std::endl;
-    result = w->predict(testImages);
 
-    for (int i = 0; i < testLabels.size(); i++) {
-        if (testLabels[i] == result[i]) {
-            count++;
-            std::cout << "Image " << i << "is " << result[i] << std::endl;
+    for (int i = 0; i < numThreads; i++) {
+        first = testImages.begin() + testImages.size() / numThreads * i;
+        last = testImages.begin() + testImages.size() / numThreads * (i + 1);
+        std::vector<std::vector<int>> partial(first, last);
+
+        prediction.push_back(new std::thread(executePartial, std::ref(partial), std::ref(resultPartial), std::ref(*w)));
+        std::cout << "Created thread " << i << std::endl;
+    }
+
+    for (int i = 0; i < numThreads; i++) {
+        prediction[i]->join();
+    }
+
+    // result = w->predict(testImages);
+
+    std::cout << "Finished, counting right answers" << std::endl;
+
+    for (int i = 0; i < resultPartial.size(); i++) {
+        for (int j = 0; j < resultPartial[i].size(); j++) {
+            if (testLabels[i * resultPartial[i].size() + j] == resultPartial[i][j]) {
+                count++;
+                // std::cout << "Image " << i << "is " << result[i] << std::endl;
+            }
         }
     }
+
     std::cout << "Rights: " << count << std::endl;
 
     // std::cout << "Imprimindo labels (" << trainingLabels.size() << ")" << std::endl;
